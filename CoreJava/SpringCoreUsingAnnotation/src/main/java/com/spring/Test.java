10fb0c089a908844acc7f9aeecfc7d8dec59e3ed
LAKSHMI A <108331858+ALPV488@users.noreply.github.com>
2024-03-28 18:48:39 +0530

    Add files via upload

---
+++ CoreJava/SpringCoreUsingAnnotation/src/main/java/com/spring/Test.java
@@ -0,0 +1,25 @@
+package com.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.spring")
+public class Test {
+	
+
+	public static void main(String[] args) {
+		
+	ApplicationContext factory = new AnnotationConfigApplicationContext(Test.class);
+	
+	System.out.println(factory.getBean("emp"));
+	
+	System.out.println(factory.getBean("add"));
+	
+
+	
+	
+	}
+}
